fac36af4b12f0c477fc3719966f79ff90b76608f
Debapriyo Saha <sahadebapriyo1990@gmail.com>
2023-05-31 09:03:26 +0530

    Add files via upload

---
+++ Maven_Final_Project _Submission.sql
@@ -0,0 +1,67 @@
+USE mavenfuzzyfactory;
+
+/*
+1. First, I’d like to show our volume growth. Can you pull overall session and order volume, 
+trended by quarter for the life of the business? Since the most recent quarter is incomplete, 
+you can decide how to handle it.
+*/ 
+
+
+
+
+/*
+2. Next, let’s showcase all of our efficiency improvements. I would love to show quarterly figures 
+since we launched, for session-to-order conversion rate, revenue per order, and revenue per session. 
+
+*/
+
+
+
+/*
+3. I’d like to show how we’ve grown specific channels. Could you pull a quarterly view of orders 
+from Gsearch nonbrand, Bsearch nonbrand, brand search overall, organic search, and direct type-in?
+*/
+
+
+
+
+;
+
+/*
+4. Next, let’s show the overall session-to-order conversion rate trends for those same channels, 
+by quarter. Please also make a note of any periods where we made major improvements or optimizations.
+*/
+
+
+
+/*
+5. We’ve come a long way since the days of selling a single product. Let’s pull monthly trending for revenue 
+and margin by product, along with total sales and revenue. Note anything you notice about seasonality.
+*/
+
+
+
+
+
+/*
+6. Let’s dive deeper into the impact of introducing new products. Please pull monthly sessions to 
+the /products page, and show how the % of those sessions clicking through another page has changed 
+over time, along with a view of how conversion from /products to placing an order has improved.
+*/
+
+-- first, identifying all the views of the /products page
+
+/*
+7. We made our 4th product available as a primary product on December 05, 2014 (it was previously only a cross-sell item). 
+Could you please pull sales data since then, and show how well each product cross-sells from one another?
+*/
+
+
+
+/*
+8. In addition to telling investors about what we’ve already achieved, let’s show them that we still have plenty 
+of gas in the tank. Based on all the analysis you’ve done, could you share some recommendations and 
+opportunities for us going forward? No right or wrong answer here – I’d just like to hear your perspective!
+*/
+
+
